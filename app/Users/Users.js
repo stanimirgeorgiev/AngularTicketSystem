edb59155ced44c29e0ccbeb8aec34051fa44128c
stanimirgeorgiev <az.stanimir@gmail.com>
2016-04-20 23:56:30 +0300

    User object and additions

--- app/Users/Users.js
+++ app/Users/Users.js
@@ -1,6 +1,41 @@
 'use strict';
 
-angular.module('Users.js', [])
-.controller('UserController',['$scope',function UserController ($scope) {
-     $scope.firstName = 'Toncho';
-}]);
+angular.module('myApp.users', [])
+
+.config(['$routeProvider', function($routeProvider) {
+    $routeProvider.when('/users', {
+        templateUrl: 'app/users/users.html',
+        controller: 'UsersController',
+    });
+}])
+
+.factory('repos', ['$http', '$q', function($http, $q) {
+
+    function getUsers(username) {
+        var deferred = $q.defer();
+
+        $http.get('http://softuni-issue-tracker.azurewebsites.net/' + username + '')
+            .then(function(result) {
+                deferred.resolve(result.data);
+            }, function(err) {
+                deferred.reject(err);
+            });
+
+        return deferred.promise;
+    }
+
+    return {
+        getUsers: getUsers
+    };
+}])
+
+.controller('UsersController', [
+    '$scope', 'users',
+    function UsersController($scope, users) {
+
+        users.getUsers('me')
+            .then(function(users) {
+                $scope.repos = users;
+            });
+    }
+]);
